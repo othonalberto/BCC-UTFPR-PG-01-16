6cc35ca71458b99aacab9de67ad9c57fe6d57ae3
scaketti <mts.scaketti@gmail.com>
2016-03-16 21:26:32 -0300

    adicionando os exercicios em java

---
+++ java/L1e3_CalcSalar.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+/*
+ * Autor: Matheus Scaketti
+ * Data: 16/03/2016
+ * 
+ * Lista: 1
+ * Exercício: 3
+ * 
+ * Descrição:
+ * O exercício pede que seja inserido o número de um vendedor, 
+ * o seu salário fixo, o total de vendas por ele efetuadas e o percentual 
+ * que ganha sobre o total de vendas. Calcular o salário total do vendedor. 
+ * Escrever número do vendedor e o salário total.
+*/
+
+public class L1e3_CalcSalar {
+
+	public static void main(String[] args) {
+		
+		//Declaração de variáveis
+		int numVend;
+		float salarFixo, totalVendas, percentVendas;
+
+		Scanner leitor = new Scanner(System.in);
+		
+		//Entrada de Dados para o programa
+		System.out.println("Digite o numero do vendedor: ");
+		numVend = leitor.nextInt();
+		
+		System.out.println("Digite o salario fixo do vendedor: ");
+		salarFixo = leitor.nextFloat();
+		
+		System.out.println("Digite o total de vendas e o percentual de ganho: ");
+		totalVendas = leitor.nextFloat();
+		percentVendas = leitor.nextFloat();
+		
+		leitor.close();
+		
+		//Cálculo do percentual sobre o total de vendas
+		salarFixo += totalVendas * (percentVendas/100);
+		
+		System.out.println("O salario total do vendedor: " + numVend + ", é: " + salarFixo);
+	}
+
+}
