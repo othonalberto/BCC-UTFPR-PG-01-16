345146f207028489e96708f3c83114aa454277f4
othonalberto <othonalberto@gmail.com>
2016-04-02 20:30:24 -0300

    add L3e1 em C

---
+++ Algoritmos/1º Período/C/L3e1_SomaX:X2.c
@@ -0,0 +1,25 @@
+/*
+ * Autor: Othon Alberto
+ * Data: 02/04/2016
+ *
+ * Lista: 3
+ * Exercício: 1
+ *
+ * Descrição:
+ * Elabore um algoritmo para resolver o valor de S, onde:
+ * S= 1/1 - 2/4 + 3/9 - 4/16 + 5/25 - 6/36 ... - 10/100
+*/
+
+#include <stdio.h>
+
+int main(){
+    int x, s = 0;
+    
+    for (x = 1; x < 11; x++){
+        
+        // variável s recebe ela mesma mais o valor de 'x' dividido por 'x' ao quadrado
+        s += (x / (x*x));
+    }
+    
+    printf("%i", s);
+}
